847dbe8e9e68104cfe3924ea281d3341504c71fb
nano <ncysatnaf@gmail.com>
2017-03-12 22:27:36 +0800

    init

---
+++ Assets/Scripts/Controller/GameController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+        
+    public static readonly string GameVersion = "0.0.1";
+
+    public static GameController Instance { get; set; }
+
+    public Message message;
+
+    public Dictionary<string, object> states;
+
+    void Start() 
+    {
+        EnableDontDestroyOnLoad();
+
+        message = new Message();
+        states = new Dictionary<string, object>();
+
+        message.on("test01", test01 => {
+            Debug.Log(test01[0]);
+        });
+        message.on("test02", test02 => {
+            Debug.Log(test02[0]);
+        });
+
+       
+        SetState("test01", "haha");
+        SetState("test02", "hehe");
+    }
+
+    public object GetState(string key)
+    {
+        if(states.ContainsKey(key))
+        {
+            return states[key];
+        } else {
+            states[key] = null;
+            return null;
+        }
+    }
+
+    public bool SetState(string key, params object[] value) {
+        states[key] = value;
+        Debug.Log("action: " + key); 
+        message.emit(key, value);
+        return true;
+    }
+
+    private void EnableDontDestroyOnLoad()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            Destroy(this);
+        }
+
+        DontDestroyOnLoad(this);
+    }
+}
---
+++ Assets/Scripts/Lib/Message.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Linq;
+
+
+public class Message {
+
+    public delegate void MessageCallback(params object[] args);
+
+    public Dictionary<string, List<MessageCallback>> messages;
+
+    public Message() {
+        messages = new Dictionary<string, List<MessageCallback>>();
+    }
+
+    public Message emit(string name, params object[] args)
+    {
+        if (messages.ContainsKey(name)){
+            foreach( var func in messages[name])
+            {
+                func(args);
+            }
+        }
+
+        return this;
+    }
+
+    public Message on(string name, MessageCallback callback)
+    {
+        if(messages.ContainsKey(name))
+        {
+            messages[name] = null;
+        }
+        else 
+        {
+            messages.Add(name, new List<MessageCallback>());
+        }
+
+        messages[name].Add(callback);
+
+        return this;
+    }
+}
